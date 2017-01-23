66ee2f03b62485e76adee5d349dca59954679d21
Jamesmalo <9jmalo@stpatrick.org>
2017-01-23 10:50:10 -0600

    day 1
    started on alternate power

---
+++ alternate power algorithm malo.js
@@ -0,0 +1,12 @@
+const Matrix = require("matrix-js");
+
+function alternate(n){
+  if(n % 2 == 0){
+    return true; //true
+  }
+  else if(n % 2 == 1){
+    return false; //odd
+  }
+  else(n == 1)
+    return n;
+}
